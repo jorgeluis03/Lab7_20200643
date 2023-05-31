1e4cc638ef9579e982d906155b1fa11e37e281df
jorgeluis <jorgel.dominguez03@gmail.com>
2023-05-31 00:21:35 -0500

    avance bosquejo

---
+++ src/main/java/com/example/lab7_20200643/controllers/UsuariosController.java
@@ -0,0 +1,13 @@
+package com.example.lab7_20200643.controllers;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/user")
+public class UsuariosController {
+    //Completar
+
+}
