612ed0a2f84ca5eb72ee55645b57d3a6ade3793e
jorgeluis <jorgel.dominguez03@gmail.com>
2023-05-31 10:11:46 -0500

    ejrcicio1 okok

--- src/main/java/com/example/lab7_20200643/controllers/UsuariosController.java
+++ src/main/java/com/example/lab7_20200643/controllers/UsuariosController.java
@@ -1,13 +1,35 @@
 package com.example.lab7_20200643.controllers;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.lab7_20200643.entity.Usuarios;
+import com.example.lab7_20200643.repository.UsuarioRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
 
 @RestController
 @CrossOrigin
-@RequestMapping("/user")
+@RequestMapping("/users")
 public class UsuariosController {
-    //Completar
+    final UsuarioRepository usuarioRepository;
+    public UsuariosController(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    @GetMapping(value = "/listar",produces = MediaType.APPLICATION_JSON_VALUE+"; charset=utf-8")
+    public ResponseEntity<HashMap<String,Object>> listar(){
+        HashMap<String,Object> responseMap = new HashMap<>();
+        responseMap.put("Usuario",usuarioRepository.findAll());
+        return ResponseEntity.ok(responseMap);
+    }
+    @PostMapping("/crear")
+    public ResponseEntity<HashMap<String,Object>> crear(@RequestBody Usuarios usuario){
+        HashMap<String,Object> responseMap= new HashMap<>();
+        usuarioRepository.save(usuario);
+        responseMap.put("Id creado",usuario.getId());
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseMap);
+    }
 
 }
